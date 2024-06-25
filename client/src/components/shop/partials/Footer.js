30e101c7ee1749e237939ef9bf40f56c1eea654d
unknown <jsahu2814@gmail.com>
2024-06-25 16:09:01 +0530

    changed footer

--- client/src/components/shop/partials/Footer.js
+++ client/src/components/shop/partials/Footer.js
@@ -2,30 +2,30 @@ import React, { Fragment } from "react";
 import moment from "moment";
 import { FaFacebookF, FaInstagram, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
 
-const Footer = (props) => {
+const Footer = () => {
   return (
     <Fragment>
       <footer
-        style={{ background: "#303031", color: "#87898A", bottom: 0}}
+        style={{ background: "#303031", color: "#87898A" }}
         className="py-8 px-4 md:px-12 text-center"
       >
-        <div className="container mx-auto flex flex-col md:flex-row justify-between items-start md:items-center">
-          <div className="mb-6 md:mb-0 md:text-center">
+        <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+          <div className="mb-6 md:mb-0 text-center md:text-left">
             <h2 className="text-lg font-semibold text-white">Contact Us</h2>
             <ul className="list-none mt-2">
-              <li className="flex items-center mb-2">
+              <li className="flex items-center justify-center md:justify-start mb-2">
                 <FaEnvelope className="mr-2" /> indiaukfit@gmail.com
               </li>
-              <li className="flex items-center">
+              <li className="flex items-center justify-center md:justify-start">
                 <FaMapMarkerAlt className="mr-2" /> Bangalore, India
               </li>
             </ul>
           </div>
-          <div className="mb-6 md:mb-0 md:text-center md:mx-4" style={{ justifyContent: "center" }}>
+          <div className="mb-6 md:mb-0 text-center">
             <h2 className="text-lg font-semibold text-white">Follow Us</h2>
-            <p className="mt-2 md:mt-0">Stay connected for updates</p>
-            <ul className="list-none flex flex-col md:flex-row mt-2 md:space-x-4" style={{ justifyContent: "center" }}>
-              <li className="flex items-center justify-center">
+            <p className="mt-2">Stay connected for updates</p>
+            <ul className="list-none flex justify-center mt-2 space-x-4">
+              <li>
                 <a
                   href="https://www.facebook.com/profile.php?id=100063891679294&mibextid=LQQJ4d"
                   target="_blank"
@@ -35,7 +35,7 @@ const Footer = (props) => {
                   <FaFacebookF size={24} />
                 </a>
               </li>
-              <li className="flex items-center justify-center">
+              <li>
                 <a
                   href="https://www.instagram.com/_ukfit_?igsh=MTN0dXk4dHFjaWticA=="
                   target="_blank"
